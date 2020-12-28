f9cf3a2bb431a43fd4b8ab3c5bb3850442117cd3
Rhys Holloway <rhyswilliamholloway@gmail.com>
2020-12-28 12:34:00 -0800

    added random number generator

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,16 @@
+extern crate rand;
+
+use crate::rand::Rng;
+
 fn main() {
     println!("Hello, world!");
     println!("hi");
+
+    let mut rand = rand::thread_rng();
+
+    let random_number = rand.gen_range(0..10);
+
+    println!("{}", random_number);
+
 }
 // joe mama balls
